d9c77f1de3bfbdc7d18899ba968458166cc91944
HasnatShahriar <shahriarhriday94@gmail.com>
2024-02-21 00:23:22 +0600

    update the score & chance part

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -10,6 +10,49 @@
 
 // ---------- functional system---------
 
+
+// document.addEventListener('keyup',function(){
+//   console.log('Alhamdulillah');
+// })
+
+
+// let count = 0;
+function handleKeyboardKeyUpEvent(event){
+  // console.log(event.key);
+  const playerPressed = event.key;
+  console.log('Player Pressed:', playerPressed);
+  const randomAlphabet = document.getElementById('random-alphabet');
+  const currentAlphabet = randomAlphabet.innerText.toLowerCase();
+  console.log(playerPressed,currentAlphabet);
+  if(playerPressed === currentAlphabet){
+    console.log('You got a point');
+    // count+=1;
+    const scoreElement = document.getElementById('score');
+    const score =scoreElement.innerText;
+    const convertedScore = parseInt(score);
+    const updatedScore = convertedScore + 1;
+    scoreElement.innerText = updatedScore;
+
+    
+    // start a new round
+    removeBackgroundColorById(currentAlphabet);
+    continueGame();
+  }else{
+    console.log('You Pressed wrong key')
+    const chanceElement = document.getElementById('chance');
+    const chance = chanceElement.innerText;
+    // console.log(typeof life);
+    const convertedChance = parseInt(chance);
+    const updatedChance = convertedChance - 1;
+    chanceElement.innerText = updatedChance;
+  }
+  
+}
+
+document.addEventListener('keyup', handleKeyboardKeyUpEvent)
+
+
+
 function continueGame(){
  const alphabet = getARandomAlphabet();
  console.log('Your Random Alphabet is :', alphabet);
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,26 +1,30 @@
-function hideElementById(elementId){
+function hideElementById(elementId) {
   const element = document.getElementById(elementId);
   element.classList.add('hidden');
 }
 
-function showElementById(elementId){
+function showElementById(elementId) {
   const element = document.getElementById(elementId);
   element.classList.remove('hidden');
 }
 
-function getARandomAlphabet(){
+function getARandomAlphabet() {
   const alphabetString = 'abcdefghijklmnopqrstuwvxyz';
   const alphabets = alphabetString.split('');
   // console.log(alphabets);
 
-  const randomNumber = Math.random()*25;
+  const randomNumber = Math.random() * 25;
   const index = Math.round(randomNumber);
   const alphabet = alphabets[index];
   // console.log(index,alphabet);
   return alphabet;
 }
 
-function setBackgroundColorById(elementId){
+function setBackgroundColorById(elementId) {
   const element = document.getElementById(elementId);
   element.classList.add('bg-orange-400');
+}
+function removeBackgroundColorById(elementId) {
+const element = document.getElementById(elementId);
+element.classList.remove('bg-orange-400');
 }
